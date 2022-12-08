6df6be45911fe3d0f660f4c3b1a00f32052709c8
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-12-08 23:11:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,3 +83,6 @@ def LOGGER(name: str) -> logging.Logger:
 
 ALWAYS_ON = os.environ.get('ALWAYS_ON', False)
 SERVER_URL = os.environ.get('SERVER_URL')
+
+about_text="<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>"
+ABOUT_TEXT = os.environ.get('ABOUT_TEXT', about_text)
